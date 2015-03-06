86535df606d6ff2f693b411d44d8dacc8d302170
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-03-05 19:37:35 -0600

    functioning modify student function for the click. works for the hardcoded method we made in ruby

--- public/global.js
+++ public/global.js
@@ -1,15 +1,43 @@
 //This file is successfully connected!
 window.onload = function () {
-var command = document.getElementById("create");
+  
+// Code for Modify Student (right now harcoded for age = 1000)
+  var command = document.getElementById("modify");
+  
+  command.addEventListener("click", getId);
+  
+  function getId() {
+    var id = prompt("Enter the student ID to be modified:");
+    //make sure id is a string
+    modifyStudent(id)
+  } //end of getId
+  
 
+  function modifyStudent(id) {
+    var request = new XMLHttpRequest;
+    
+    var query = "http://localhost:4567/students/" + id + "//modify"
+    
+    request.open("get", query);
+    request.send();
+    alert("Student Modified!");
+  } //end of modifyStudent
 
-function createStudent() {
-  var request = new XMLHttpRequest;
-  request.open("get", "http://localhost:4567/students/create");
-  request.send();
-  alert("Created Student!");
-}
+// command.addEventListener("click", getId, createStudent);
+  
 
-command.addEventListener("click", createStudent);
-}
-// document.getElementById("create").addEventListener("click", createStudent());
+// /////////////////////////////////
+//Code for Create student link
+// /////////////////////////////////
+
+  var command = document.getElementById("create");
+
+  function createStudent() {
+    var request = new XMLHttpRequest;
+    request.open("get", "http://localhost:4567/students/create");
+    request.send();
+    alert("Created Student!");
+  } //end of createStudent
+
+  command.addEventListener("click", createStudent);
+} // end of window.onload
